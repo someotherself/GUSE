8a9ab790cef9764a5cf8d548ec2f7e9708d41c48
someotherself <romancristianv@gmail.com>
2025-08-19 17:16:33 +0300

    read method added

--- src/fs/ops/read.rs
+++ src/fs/ops/read.rs
@@ -1,22 +1,45 @@
-#![allow(unused_variables)]
+use std::os::unix::fs::FileExt;
+
+use anyhow::{anyhow, bail};
+
 use crate::fs::GitFs;
 
 pub fn read_live(
     fs: &GitFs,
-    ino: u64,
+    _ino: u64,
     offset: u64,
     buf: &mut [u8],
     fh: u64,
 ) -> anyhow::Result<usize> {
-    todo!()
+    let guard = fs.handles.read().map_err(|_| anyhow!("Lock poisoned."))?;
+    let ctx = guard
+        .get(&fh)
+        .ok_or_else(|| anyhow!("Handle does not exist"))?;
+    if !ctx.write {
+        bail!("Write not permitted")
+    };
+    if !ctx.file.is_file() {
+        bail!("Invalid handle.")
+    }
+    Ok(ctx.file.read_at(buf, offset)?)
 }
 
 pub fn read_git(
     fs: &GitFs,
-    ino: u64,
+    _ino: u64,
     offset: u64,
     buf: &mut [u8],
     fh: u64,
 ) -> anyhow::Result<usize> {
-    todo!()
+    let guard = fs.handles.read().map_err(|_| anyhow!("Lock poisoned."))?;
+    let ctx = guard
+        .get(&fh)
+        .ok_or_else(|| anyhow!("Handle does not exist"))?;
+    if !ctx.write {
+        bail!("Write not permitted")
+    };
+    if !ctx.file.is_blob() {
+        bail!("Invalid handle.")
+    }
+    Ok(ctx.file.read_at(buf, offset)?)
 }
