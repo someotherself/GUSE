03c92c7036d0d1102121496376eb330ae55cae5c
someotherself <romancristianv@gmail.com>
2025-08-20 18:26:03 +0300

    initialized

---
+++ src/fs/ops/rmdir.rs
@@ -0,0 +1,9 @@
+use crate::fs::GitFs;
+
+pub fn rmdir_repo(_fs: &GitFs, _parent: u64, _name: &str) -> anyhow::Result<()> {
+    todo!()
+}
+
+pub fn rmdir_live(_fs: &GitFs, _parent: u64, _name: &str) -> anyhow::Result<()> {
+    todo!()
+}
