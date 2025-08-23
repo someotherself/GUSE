484ad77b7c7c99e6b73d4ce34c6ff32c1fcc98eb
someotherself <romancristianv@gmail.com>
2025-08-23 09:13:59 +0300

    comment write_nodes_to_db

--- src/fs.rs
+++ src/fs.rs
@@ -1074,7 +1074,7 @@ impl GitFs {
         conn.get_name_from_db(ino)
     }
 
-    /// nodes = Vec<entry inode, entry name, entry attr>
+    /// nodes = Vec<parent inode, entry name, entry attr>
     fn write_inodes_to_db(&self, nodes: Vec<(u64, String, FileAttr)>) -> anyhow::Result<()> {
         if nodes.is_empty() {
             return Ok(());
