45de2547040b52a341831d7aa62dd3cd92a08af3
someotherself <romancristianv@gmail.com>
2025-08-27 13:49:59 +0300

    next_ino deadlock fix

--- src/fs.rs
+++ src/fs.rs
@@ -1012,12 +1012,14 @@ impl GitFs {
     fn next_inode_checked(&self, parent: u64) -> anyhow::Result<u64> {
         let mut inode = self.next_inode_raw(parent)?;
         let repo = self.get_repo(inode)?;
+        let mut repo = repo.lock().map_err(|_| anyhow!("Lock poisoned"))?;
         while {
-            let repo = repo.lock().map_err(|_| anyhow!("Lock poisoned"))?;
             repo.res_inodes.contains(&inode)
         } {
             inode = self.next_inode_raw(parent)?;
         }
+        repo.res_inodes.insert(inode);
+        info!("Issuing ino {inode}");
         Ok(inode)
     }
 
