8a9a074163dca472f0f0d50f9c5c055404e31d26
someotherself <romancristianv@gmail.com>
2025-09-02 13:26:19 +0300

    deleting live folder

--- src/fs/ops/rmdir.rs
+++ src/fs/ops/rmdir.rs
@@ -27,10 +27,18 @@ pub fn rmdir_repo(fs: &GitFs, parent: u64, name: &str) -> anyhow::Result<()> {
     if !fs.is_dir(attr.ino)? {
         bail!("Not a directory")
     }
+    let live_ino = fs.get_live_ino(parent);
+    if attr.ino == live_ino {
+        {
+            let repo = fs.get_repo(parent)?;
+            let mut repo = repo.lock().map_err(|_| anyhow!("Lock poisoned"))?;
+            repo.live_exists = false;
+        }
+        return Ok(());
+    }
 
     let entries = fs.readdir(attr.ino)?;
     let mut entries_len = entries.len();
-    let live_ino = fs.get_live_ino(parent);
     for entry in &entries {
         if entry.ino == live_ino || entries_len > 0 {
             entries_len -= 1;
