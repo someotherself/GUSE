b8e0357c940a0550c411c484d3fb8fa0a92c120a
someotherself <romancristianv@gmail.com>
2025-12-04 18:24:09 +0200

    updates to latest functionality

--- src/fs/builds/chase.lua
+++ src/fs/builds/chase.lua
@@ -9,9 +9,19 @@ local run_mode = "Continuous"
 -- Sets the build mode. Can be "FirstFailure" or "Continuous"
 local stop_mode = "Continuous"
 
+  -- HOW TO ADD COMMITS:
+  -- single commits:            local commits = {"hash", "hash", "hash",}
+  -- a complete branch or Pr:   local commits = {Branch = "branch_name"} / { Pr = "Pr_number" }
+  -- a range of commits:        local commits = {Range = "hash...hash"}
+  -- Or any combination of them
+
 -- Load functions
-for _, oid in ipairs(commits) do
-  cfg.add_commit(oid)
+for input_type, oid in pairs(commits) do
+  if type(input_type) == "number" then 
+    cfg.add_commit("commit", oid)
+  else
+    cfg.add_commit(input_type, oid)
+  end
 end
 
 for _, command in ipairs(commands) do
